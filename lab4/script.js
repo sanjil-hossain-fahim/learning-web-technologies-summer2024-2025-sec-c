2f6394339fa02fe8da9bc761f276ca5e33fc5188
sanjil-hossain-fahim <sanjilhossanfahim@gmail.com>
2025-08-02 16:43:32 +0600

    cal rev

--- lab4/script.js
+++ lab4/script.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function()
+ {
     
     const calculatorContainer = document.getElementById('calculator-container');
     
